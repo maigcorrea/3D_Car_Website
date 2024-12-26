b2780f8d6248afde3af0f5b0076bb85dde96ee48
Maite <anamaitegarciacorrea@gmail.com>
2024-12-26 23:49:17 +0100

    Se añaden luces a la escena 3D

--- app.js
+++ app.js
@@ -50,6 +50,32 @@ document.getElementById("dCanvas").appendChild(renderer.domElement); //renderer.
 
 //set Camera
 camera.position.set(5, 0, 1); //x, y, z
+//Add light in 3d model - Estas luces juntas ayudan a crear una iluminación realista y dinámica para el modelo 3D
+//Creación de luz ambiental que ilumina toda la escena de manera uniforme. Esta luz no tiene una dirección específica, por lo que afecta a todos los objetos por igual.
+let ambientLigth=new THREE.AmbientLight(0x404040,1);//0x404040: El color de la luz, en formato hexadecimal. 1: La intensidad de la luz, 1 es el valor máximo
+scene.add(ambientLigth);//Añade la luz ambiental a la escena
+//Creación de luz direccional, que ilumina en una dirección específica.
+let directionLight=new THREE.DirectionalLight(0xffffff,1);
+directionLight.position.set(0,1,0);//Establece la posición de la luz direccional en las coordenadas (0, 1, 0). Esta posición no es tan importante en una luz direccional como lo sería en una luz puntual, ya que la luz direccional no tiene una posición fija, sino que se proyecta desde una dirección.
+scene.add(directionLight);
+//Creación de luces puntuales. Se crea una luz puntual (PointLight), que emite luz en todas las direcciones desde un solo punto en el espacio 3D
+let light=new THREE.PointLight(0x4c4c4c, 10);
+light.position.set(0,300,500);//Establece la posición de la luz en las coordenadas (0, 300, 500). Esto la coloca a una gran altura en el eje Y (300 unidades) y un poco alejada en el eje Z (500 unidades)
+scene.add(light);
+
+//Otras pointLight con posiciones diferentes
+let light2=new THREE.PointLight(0x4c4c4c, 10);
+light2.position.set(500,100,0);
+scene.add(light2);
+
+let light3=new THREE.PointLight(0x4c4c4c, 10);
+light3.position.set(0,100,-500); //-500 en el eje z: La luz se posiciona a 500ud detrás de la cámara del eje z
+scene.add(light3);
+
+let light4=new THREE.PointLight(0x4c4c4c, 10);
+light4.position.set(-500,300,500);//-500 en el eje x: La luz está a la izquierda de la escena. En términos de coordenadas, está 500 unidades más lejos del centro en la dirección negativa del eje X.
+scene.add(light4);
+
 //Add controls to the camera - allow the camera to move(rotate, zoom, scroll)
 controls=new OrbitControls(camera, renderer.domElement); //camera: Se le pasa la cámara que se moverá. renderer.domElement: Se le pasa el canvas donde se renderiza la escena, lo que permite que los controles respondan a las interacciones del usuario en ese elemento.
 //Render the scene
